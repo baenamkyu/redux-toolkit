5752763d4331f4b9b7ef12747e974875a0c0f605
baenamkyu <baenamkyu@gmail.com>
2022-10-03 10:10:07 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counterSlice',
+  initialState: { value: 0 },
+  reducers: {
+    up: (state, action) => {
+      state.value = state.value + action.payload;
+    },
+  },
+});
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => {
+    return state.counter.value;
+  });
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(counterSlice.actions.up(2));
+        }}
+      >
+        +
+      </button>
+      {count}
+    </div>
+  );
+}
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
   );
 }
